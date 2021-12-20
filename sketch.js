369aed73551d2308555d73114d16b6db8ba3610d
Jihyun-Ghim <84126943+Jihyun-Ghim@users.noreply.github.com>
2021-12-20 23:21:51 +0900

    create three circle and change

--- sketch.js
+++ sketch.js
@@ -9,10 +9,8 @@ function setup() {
 
   angleMode(DEGREES);
 
-  // testSound = loadSound("testSound.mp3", loaded);
-
-  // innerSound = loadSound("innerSound.mp3", loaded);
-  // movingSound = loadSound("movingSound.mp3", loaded);
+  innerSound = loadSound("innerSound.mp3", loaded);
+  movingSound = loadSound("movingSound.mp3", loaded);
   outsideSound = loadSound("outsideSound.m4a", loaded);
 
   sliderVolume = createSlider(0, 1, 0.5, 0.01);
@@ -22,26 +20,38 @@ function setup() {
   amp = new p5.Amplitude();
 }
 
-function loaded() {
-  // testSound.play();
+// function loaded() {
+//   innerSound.play();
+//   movingSound.play();
+//   outsideSound.play(); //사운드 재생
+// }
 
-  // innerSound.play();
-  // movingSound.play();
-  outsideSound.play(); //사운드 재생
+function mousePressed(){
+  innerSound.play();
+  movingSound.play();
+  outsideSound.play();
 }
 
 function draw() {
   background(0);
 
+  innerSound.setVolume(sliderVolume.value());
+  movingSound.setVolume(sliderVolume.value());
   outsideSound.setVolume(sliderVolume.value());
+
+  innerSound.rate(sliderRate.value());
+  movingSound.rate(sliderRate.value());
   outsideSound.rate(sliderRate.value());
+
+  innerSound.pan(sliderPan.value());
+  movingSound.pan(sliderPan.value());
   outsideSound.pan(sliderPan.value()); //슬라이더 속성 부여
 
   var vol = amp.getLevel();
   volhistory.push(vol);
-  stroke(255);
   noFill();
 
+  stroke(80);
   translate(width / 2, height / 2);
   beginShape();
   for (var i = 0; i < 360; i++){
@@ -51,7 +61,30 @@ function draw() {
 
     vertex(x, y);
   }
-  endShape();
+  endShape(); //biggest circle
+
+  stroke(140);
+  beginShape();
+  for (var i = 0; i < 360; i++){
+    var r = map(volhistory[i], 0, 1, 100, 200);
+    var x = r * sin(i);
+    var y = r * cos(i);
+
+    vertex(x, y);
+  }
+  endShape(); //middle circle
+
+  stroke(255);
+  fill(255);
+  beginShape();
+  for (var i = 0; i < 360; i++){
+    var r = map(volhistory[i], 0, 1, 10, 100);
+    var x = r * cos(i);
+    var y = r * sin(i);
+
+    vertex(x, y);
+  }
+  endShape(); //smallest circle
 
   if (volhistory.length > 360) {
     volhistory.splice(0, 1);
